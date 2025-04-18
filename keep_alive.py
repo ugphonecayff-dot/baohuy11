8025d6af310b166c2b0a3960397220f760f24ab2
huydoan633 <hbaohuy46@gmail.com>
2025-04-18 11:56:07 +0700

    Update setup.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,2 @@
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+pip install python-telegram-bot
+pip3 install python-telegram-bot
