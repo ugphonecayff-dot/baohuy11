af84e995d87a436ddfa57bb21eedf3ec2aae5458
huydoan633 <hbaohuy46@gmail.com>
2025-06-23 01:35:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,3 @@
-API_ID = 5736655322  # Thay bằng API ID thật
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "6367532329:AAEuSSv8JuGKzJQD6qI431udTvdq1l25zo0"
-
-LIKE_API_KEY = "vlong"
-LIKE_API_URL = "https://dichvukey.site/likeff2.php"
-
-LIKE_COOLDOWN = 60  # Giây
-
-DEFAULT_REACT_STATUS = True
-
-EMOJIS = [
-    "🥰", "❤️", "💋", "👍", "👏", "😂", "😊", "🎉", "🤔", "😎",
-    "🙏", "💯", "🔥", "😍", "🤣", "🙂", "🤗", "🥳", "😇", "💖"
-]
+# config.py
+BOT_TOKEN = "6367532329:AAGJh1RnIa-UZGBUdzKHTy3lyKnB81NdqjM"  # Thay token bot bạn tạo
+ADMIN_IDS = [5736655322]  # ID Telegram của admin (dạng số)
