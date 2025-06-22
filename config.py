d031fd15f4b9b7bb6c2cbe0e33ef6f0fe9c39237
huydoan633 <hbaohuy46@gmail.com>
2025-06-23 02:29:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,3 @@ BOT_TOKEN = "6367532329:AAGJh1RnIa-UZGBUdzKHTy3lyKnB81NdqjM"
 ADMIN_IDS = [5736655322]  # Thay bằng Telegram ID của bạn
 MB_ACCOUNT = "0971487462"
 MB_BANK_CODE = "mbbank"
-WEBHOOK_SECRET = "my-secret-token"
