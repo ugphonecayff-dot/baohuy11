6f8bf046deb3b03a6fd8aee19a301d3df5aad984
huydoan633 <hbaohuy46@gmail.com>
2025-06-23 02:15:35 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "Bot Huy running!"
 
 def keep_alive():
-    t = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
-    t.start()
+    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
