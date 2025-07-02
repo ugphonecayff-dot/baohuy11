4aea77aabca5d975c0ab8191b8f23d7f6159bf69
huydoan633 <hbaohuy46@gmail.com>
2025-07-02 23:22:11 +0700

    Update and rename config.py to .env

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-BOT_TOKEN = "6367532329:AAGJh1RnIa-UZGBUdzKHTy3lyKnB81NdqjM"
-ADMIN_IDS = [5736655322]  # Thay bằng Telegram ID của bạn
-MB_ACCOUNT = "0971487462"
-MB_BANK_CODE = "mbbank"
